e76b587091ed3385bc599729d21c3744e4c01dd3
SAI DISHANTH BANOTH <120818369+Dishanth22@users.noreply.github.com>
2025-12-12 23:00:15 +0530

    Refactor tests to use dash_app fixture

--- test_dash_app.py
+++ test_dash_app.py
@@ -1,23 +1,24 @@
 import pytest
-from dash.testing.application_runners import import_app
+from app import app
 
-def test_header_present(dash_duo):
-    app = import_app("app")
-    dash_duo.start_server(app)
+@pytest.fixture
+def dash_app():
+    return app
 
+# 1️⃣ Test that header is present
+def test_header_present(dash_duo, dash_app):
+    dash_duo.start_server(dash_app)
     header = dash_duo.find_element("h1")
-    assert "Pink Morsel Sales Visualiser" in header.text
+    assert header.text == "Pink Morsel Sales Visualiser"
 
-def test_graph_present(dash_duo):
-    app = import_app("app")
-    dash_duo.start_server(app)
-
+# 2️⃣ Test that visualisation (graph) is present
+def test_graph_present(dash_duo, dash_app):
+    dash_duo.start_server(dash_app)
     graph = dash_duo.find_element("#sales-line-chart")
     assert graph is not None
 
-def test_region_picker_present(dash_duo):
-    app = import_app("app")
-    dash_duo.start_server(app)
-
-    picker = dash_duo.find_element("#region-filter")
-    assert picker is not None
+# 3️⃣ Test that region picker is present
+def test_region_picker_present(dash_duo, dash_app):
+    dash_duo.start_server(dash_app)
+    radio = dash_duo.find_element("#region-picker")
+    assert radio is not None
