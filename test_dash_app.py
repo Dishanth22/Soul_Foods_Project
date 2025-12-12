0f0e4c08c6e57fa65c7631f1ad62a4dcf1f0c0bc
SAI DISHANTH BANOTH <120818369+Dishanth22@users.noreply.github.com>
2025-12-12 23:06:05 +0530

    Update test_dash_app.py

--- test_dash_app.py
+++ test_dash_app.py
@@ -5,19 +5,16 @@ from app import app
 def dash_app():
     return app
 
-# 1️⃣ Test that header is present
 def test_header_present(dash_duo, dash_app):
     dash_duo.start_server(dash_app)
     header = dash_duo.find_element("h1")
-    assert header.text == "Pink Morsel Sales Visualiser"
+    assert "Pink Morsel Sales Visualiser" in header.text
 
-# 2️⃣ Test that visualisation (graph) is present
 def test_graph_present(dash_duo, dash_app):
     dash_duo.start_server(dash_app)
     graph = dash_duo.find_element("#sales-line-chart")
     assert graph is not None
 
-# 3️⃣ Test that region picker is present
 def test_region_picker_present(dash_duo, dash_app):
     dash_duo.start_server(dash_app)
     radio = dash_duo.find_element("#region-picker")
